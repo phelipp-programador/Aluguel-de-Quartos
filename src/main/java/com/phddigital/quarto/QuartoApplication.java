584cfc6b8892a239ca7e690b50cfb7a788eed4b8
phelipp <phelippnd.programador@gmail.com>
2021-01-04 10:43:56 -0300

    Inicio

---
+++ src/main/java/com/phddigital/quarto/QuartoApplication.java
@@ -0,0 +1,13 @@
+package com.phddigital.quarto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phddigital/quarto/QuartoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phddigital.quarto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
